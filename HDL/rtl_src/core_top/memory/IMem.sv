8f67f6c208e7dc55d5a39f45a1da78a773fb72b9
chrisshakkour <chrisshakkour@gmail.com>
2021-10-31 20:26:16 +0200

    creating instruction memory block, adding simulation too

--- HDL/rtl_src/core_top/memory/IMem.sv
+++ HDL/rtl_src/core_top/memory/IMem.sv
@@ -23,8 +23,8 @@
 
 module IMem
   #(
-    parameter integer IMEM_SIZE=(2**15),
-    parameter integer ADDR_W=32,
+    parameter integer IMEM_SIZE=memory_pkg::IMEM_BYTES,
+    parameter integer ADDR_W=memory_pkg::MEM_ADDR_WIDTH,
     localparam integer WORD_W=memory_pkg::MEM_WORD_WIDTH
     )
    (
@@ -39,15 +39,18 @@ module IMem
    // from 0x0000_000 and ends 
    // in 0x0000_4000, ~16KByte
    logic [7:0] 		      imem_ram [IMEM_SIZE-1:0]; 
-   logic 		      valid_req;
+   logic 		      valid_addr;
 
    assign valid_addr = (addr < IMEM_SIZE);
    
    always_ff @(posedge clk)
-     addr_err <= ~valid_req;
-   
+     addr_err <= (req & ~valid_addr);
+
    always_ff @(posedge clk)
-     if(req & valid_addr) data <= imem_ram[addr:+4];
+     if(req & valid_addr) data <= {imem_ram[addr+3],
+				   imem_ram[addr+2],
+				   imem_ram[addr+1],
+				   imem_ram[addr]};
 
 /*///////////////////////////////////////////////////
     _    ___  ___  ___  ___  _____  ___  ___   _  _
@@ -56,18 +59,30 @@ module IMem
  /_/ \_\|___/|___/|___||_|_\  |_|  |___|\___/ |_|\_|
 
 *////////////////////////////////////////////////////
-   
+
    /* pragma translate_off */
    
-`ifndef DISABLE_ASSERTIONS
+`ifdef DISABLE_ASSERTIONS
+`else
+
+   // modelsim does not support assertions
+   //  except immediate :( following override:
+   // non SYNTH code
+   always_ff @(posedge clk)
+     if(req) begin
+	assert((addr % 4)==0);
+	assert(valid_addr);
+     end
+
+   // not supported
    property unvalid_instruction;
-     @(posedge clk) req |-> ((addr % 4)==0);
+      @(posedge clk) req |-> ((addr % 4)==0);
    endproperty // unvalid_instruction
    
    property unvalid_address;
       @(posedge clk) req |-> valid_addr;
-   endproperty // unvalid_address
-     
+   endproperty // unvalid_address   
+   
    requested_instruction_unvalid: assert property(unvalid_instruction);
    requested_address_out_of_range: assert property(unvalid_address);
 `endif   
---
+++ verif/rtl_sim/IMem_TB.sv
@@ -0,0 +1,80 @@
+/*
+
+description: 
+design file: /home/christians/git/RV32I-MAF-project/HDL/rtl_src/core_top/memory/IMem.sv
+
+*/
+
+`timescale 1ns/1ps
+
+module IMem_TB();
+
+   parameter string IMEM_IMAGE;
+   parameter integer IMEM_SIZE=memory_pkg::IMEM_BYTES;
+   parameter integer ADDR_W=memory_pkg::MEM_ADDR_WIDTH;
+   localparam integer WORD_W=memory_pkg::MEM_WORD_WIDTH;
+   
+   localparam HALF_CLK=5; 
+   localparam PERIOD=(2*HALF_CLK); 
+   
+   logic 	      clk;
+   logic 	      req;
+   logic [ADDR_W-1:0] addr;
+   logic 	      addr_err;
+   logic [WORD_W-1:0] data;
+
+   logic 	      clk_en;
+   always #HALF_CLK clk = (clk_en) ? ~clk : clk; 
+   
+   IMem #
+     (
+      .IMEM_SIZE(IMEM_SIZE),
+      .ADDR_W(ADDR_W)
+      )
+   DUT_IMem
+   (
+    // inputs
+    .clk(clk),
+    .req(req),
+    .addr(addr),
+    // outputs
+    .addr_err(addr_err),
+    .data(data)
+    );
+   
+   task init();
+      clk='0;
+      clk_en='0;
+      req='0;
+      addr='0;
+   endtask // init()
+   
+   task reset();
+   endtask // reset()
+
+   task load_mem();
+      $display("Reading instruction memory image from file: %s", IMEM_IMAGE);
+      $readmemh(IMEM_IMAGE, DUT_IMem.imem_ram, 0);
+   endtask // load_mem
+   
+   task read_instruction(int address); //32bit unsigned address;
+      req=1'b1;
+      addr=address;
+      #(PERIOD) req=1'b0;
+   endtask // read_instruction   
+   
+   initial begin
+      #(PERIOD) init();
+      #(2*PERIOD) load_mem;
+      #(2*PERIOD) clk_en=1'b1;
+      #(2.5*PERIOD);
+      for(int i=0; i<20; i++)
+	read_instruction(32'h00000000+(4*i));
+      #(2*PERIOD) read_instruction(32'h00010000);
+      //assert shall fail on unavailable address
+      #(2*PERIOD) read_instruction(32'h00004001);
+      // assert shall fail on unvalid instruction,
+      // address shall be divided by 4 with zero remainder.
+      #(4*PERIOD) $finish;
+   end
+endmodule
