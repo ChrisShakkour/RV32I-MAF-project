47688dc53d6501ebf679394bd7ac230986dc8a58
chrisshakkour <chrisshakkour@gmail.com>
2021-12-18 00:40:06 +0200

    new Decoder designed, few rtl changes and upgrades

--- HDL/rtl_src/core_top/core/InstructionFetch.sv
+++ HDL/rtl_src/core_top/core/InstructionFetch.sv
@@ -7,36 +7,37 @@
 
 module InstructionFetch 
   import instructions_pkg::*;
-  (
-   input  logic 	    clk,
-   input  logic 	    rstn,
-   input  logic             sel_next_pc,
-   input  logic [XLEN-1:0]  alu_pc,
+   (
+    input logic 	    clk,
+    input logic 	    rstn,
+    input logic 	    sel_next_pc,
+    input logic [XLEN-1:0]  alu_pc,
+    
+    output logic 	    inst_request, //instruction request
+    output logic [XLEN-1:0] pc,           //instruction address
+    output logic [XLEN-1:0] pc_out,       //pc   going to pipeline
+    output logic [XLEN-1:0] pc_pls4_out   //pc+4 going to pipeline
+    );
 
-   output logic 	    inst_request,
-   output logic [XLEN-1:0]  pc,
-   output logic [XLEN-1:0]  pc_pls4   
-     );
+   logic [XLEN-1:0] 	    pc_nxt;
+   logic [XLEN-1:0] 	    pc_pls4;
    
-   logic [XLEN-1:0] 	       pc_nxt;
-   logic [XLEN-1:0] 	       pc_pls4_nxt;
+   assign pc_pls4 = pc + 4; 
 
-   assign pc_pls4_nxt = pc + 4; 
-   assign pc_nxt = sel_next_pc ? alu_pc : pc_pls4_nxt;
-   assign inst_request = 1'b1;
+   //TODO: create a state machine for controling the PC
+   //states: IDLE, CPU_GO, FLUSH, STALL, CPU_DONE
+   assign pc_nxt = sel_next_pc ? alu_pc : pc_pls4;
+   assign inst_request = 1'b1; 
+
+   //######## REGISTERS ########################
    
-
-//######## REGISTERS ########################
-
-     always_ff @(posedge clk or negedge rstn) 
-       begin
-         if (!rstn)
-           pc <= '0;
-         else
-           pc <= pc_nxt;
-       end
-
-       always_ff @(posedge clk)
-	   pc_pls4 <= pc_pls4_nxt;
-
+   always_ff @(posedge clk or negedge rstn)
+     if (!rstn) pc <= '0;
+     else       pc <= pc_nxt;
+   
+   always_ff @(posedge clk) begin
+      pc_out      <= pc;
+      pc_pls4_out <= pc_pls4;
+   end
+   
 endmodule // InstructionFetch
--- HDL/rtl_src/core_top/core/WriteBack.sv
+++ HDL/rtl_src/core_top/core/WriteBack.sv
@@ -4,26 +4,45 @@
  
  */
 module WriteBack
-  import instructions_pkg::*;	  
-  (
-     input logic                        clk,
-     input logic                        rstn,
-     input logic  [XLEN-1:0]            AluData,
-     input logic  [XLEN-1:0]            pc_pls4,
-     input logic [MSB_REG_FILE-1:0]     rd,     
-     input logic                        sel_next_pc,
-     input logic                        ctrl_reg_wr,
+  import instructions_pkg::*;
+   import memory_pkg::*;
+   import control_pkg::*;
+   (
+    input logic 		     clk,
+    input logic 		     rstn,
+    input logic [XLEN-1:0] 	     AluData,
+    input logic [XLEN-1:0] 	     pc_pls4,
+    input logic [MSB_REG_FILE-1:0]   rd, 
+    input logic 		     sel_next_pc,
 
-     output logic [XLEN-1:0]            rdData,
-     output logic [MSB_REG_FILE-1:0]    rdOut,
-     output logic                       writeEn
-     );
+    // TODO: add data memory 
+    // address error signal + logic
+    input logic [MEM_WORD_WIDTH-1:0] dmem_load_data,
+    //
+    input logic 		     ctrl_reg_wr,
+    input 			     e_regfile_wb_sel ctrl_wb_to_rf_sel_in,
+   
+   
+    output logic [XLEN-1:0] 	     rdData,
+    output logic [MSB_REG_FILE-1:0]  rdOut,
+    output logic 		     writeEn
+    );
 
-     assign rdData     = sel_next_pc ? pc_pls4 : AluData;
-     assign writeEn    = ctrl_reg_wr;
-     assign rdOut      = rd;     
+   //assign rdData     = sel_next_pc ? pc_pls4 : AluData;
+   assign writeEn    = ctrl_reg_wr;
+   assign rdOut      = rd;     
 
-//######### REGISTERS ##############
-
-
+   always_comb begin
+      unique case(ctrl_wb_to_rf_sel_in)
+	WB_ALU_OUT:
+	  rdData = AluData;
+	WB_MEM_LOAD:
+	  rdData = dmem_load_data;
+	WB_PC_PLS4:
+	  rdData = pc_pls4;
+	default
+	  rdData = AluData;
+      endcase // unique case (ctrl_wb_to_rf_sel_in)
+   end // always_comb
+   
 endmodule // WriteBack
